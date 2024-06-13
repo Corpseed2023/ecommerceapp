b2ae4dcf086741d3e27a0b1955c1f7f945820d03
corpseed <132264141+Corpseed2023@users.noreply.github.com>
2024-06-13 13:19:37 +0530

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './Login/LoginPage';
+import MainPage from './Main/MainPage';
+import SignupPage from './Login/SignupPage';
+import NoPageFound from './Components/NoPageFound';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Ecommerce</h1>
-    </div>
+  <BrowserRouter>
+  <Routes>
+  <Route path="/" element={<MainPage />} />
+  <Route path="/signup" element={<SignupPage />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="*" element={<NoPageFound />} />
+
+  </Routes>
+  </BrowserRouter>
   );
 }
 
